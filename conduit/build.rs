c9ade9bf9c8613caa2ee24707fb08a04f50ce3f5
Alexander McCord <alex@mccord.dev>
2022-04-17 19:43:15 -0700

    Actually be cross platform. :(

--- conduit/build.rs
+++ conduit/build.rs
@@ -1,20 +1,43 @@
+use std::path::PathBuf;
+
+struct Location {
+    build_dir: PathBuf,
+    name: &'static str,
+}
+
+#[cfg(target_os = "windows")]
+fn locate_executable(dest: PathBuf, profile: &str) -> Location {
+    let backend_build_dir = dest.join("build").join(profile);
+
+    if !backend_build_dir.join("backend.exe").exists() {
+        panic!("No backend.exe found in {}, did compilation fail?", backend_build_dir.display());
+    }
+
+    Location { build_dir: backend_build_dir, name: "backend.exe" }
+}
+
+// Not yet sure if this works on MacOS too. I don't have a MacOS laptop upon which to test on.
+#[cfg(not(target_os = "windows"))]
+fn locate_executable(dest: PathBuf, _profile: &str) -> Location {
+    let backend_build_dir = dest.join("build");
+
+    if !backend_build_dir.join("backend").exists() {
+        panic!("No backend found in {}, did compilation fail?", backend_build_dir.display());
+    }
+
+    Location { build_dir: backend_build_dir, name: "backend" }
+}
+
 fn main() {
     let mut config = cmake::Config::new("../backend");
     config.build_target("backend");
     let dest = config.build();
 
-    let backend_build_dir = dest.join("build").join(config.get_profile());
-    let backend_exe_name = if backend_build_dir.join("backend.exe").exists() {
-        "backend.exe"
-    } else if backend_build_dir.join("backend").exists() {
-        "backend"
-    } else {
-        unreachable!();
-    };
-
-    // Needless to say, I am not a fan of this.
     let deps_dir = dest.parent().unwrap().parent().unwrap().parent().unwrap().join("deps");
-    if let Err(e) = std::fs::copy(backend_build_dir.join(backend_exe_name), deps_dir.join(backend_exe_name)) {
+    let Location { build_dir, name } = locate_executable(dest, config.get_profile());
+    let backend_binary_path = build_dir.join(name);
+
+    if let Err(e) = std::fs::copy(backend_binary_path, deps_dir.join(name)) {
         panic!("Failed to copy: {}", e);
     }
 }
--- conduit/src/conduit.rs
+++ conduit/src/conduit.rs
@@ -5,9 +5,20 @@ pub struct Conduit {
 }
 
 impl Conduit {
+    #[cfg(target_os = "windows")]
+    fn launch() -> Command {
+        Command::new("backend.exe")
+    }
+
+    #[cfg(not(target_os = "windows"))]
+    fn launch() -> Command {
+        let dir = std::env::current_exe().unwrap();
+        let cwd = dir.parent().unwrap();
+        Command::new(cwd.join("backend"))
+    }
+
     pub fn new() -> Conduit {
-        let mut command = Command::new("backend.exe");
-        let child = command
+        let child = Conduit::launch()
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
             .stderr(Stdio::piped())
